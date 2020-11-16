0ea328feadfceddd99bc06d90b7a2481a217b5fe
Matheus Duarte Benedito <matheus.benedito@mjv.net>
2020-11-15 21:21:16 -0300

    atualização de código

--- cypress/integration/tests.js
+++ cypress/integration/tests.js
@@ -1,10 +1,5 @@
 /// <reference types="cypress" />
-
-
-
-
 import { fuelSavings } from '../pages/fuelSavings';
-
 
 var mpg1 = 10
 var mpg2 = 18
@@ -13,11 +8,9 @@ var price2 = 4.99
 var milhas = 1000
 var data = new Date();
 
-
-
 context('tests ', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/fuel-savings')
+    cy.visit(fuelSavings.base_url)
   })
 
   // https://on.cypress.io/interacting-with-elements
@@ -30,12 +23,11 @@ context('tests ', () => {
       cy.get(fuelSavings.milesDriven).clear().type(milhas).should("have.value", milhas)
       cy.get(fuelSavings.dropMilesDriven).select('week').should("have.value", "week")
       cy.get(fuelSavings.dropMilesDriven).select('month').should("have.value", "month")
-      cy.get(fuelSavings.dropMilesDriven).select('Year').should("have.value", "year")
+      cy.get(fuelSavings.dropMilesDriven).select('year').should("have.value", "year")
       cy.get(fuelSavings.save).click
     }) 
 
-
-    it('Teste dos campos obrigatorios', () => { 
+    it('Teste dos campos obrigatórios', () => { 
 
       cy.get(fuelSavings.save).click
       cy.get(fuelSavings.results).should('not.be.visible') 
@@ -68,45 +60,34 @@ context('tests ', () => {
       cy.get(fuelSavings.dropMilesDriven).select('week')
       cy.get(fuelSavings.save).click
       cy.get(fuelSavings.results).should('not.be.visible')
-
-
       })
 
       it('Teste para não aceitar texto', () => { 
-        cy.get(fuelSavings.newMpg).clear().type('10,0')
-        cy.get(fuelSavings.tradeMpg).clear().type('18,0')
-        cy.get(fuelSavings.newPpg).clear().type('3,99')
-        cy.get(fuelSavings.tradePpg).clear().type('4,99')
-        cy.get(fuelSavings.milesDriven).clear().type('1000,0')
-        cy.get(fuelSavings.save).click
-        cy.get(fuelSavings.results).should('not.be.visible')
+      cy.get(fuelSavings.newMpg).clear().type('10,0')
+      cy.get(fuelSavings.tradeMpg).clear().type('18,0')
+      cy.get(fuelSavings.newPpg).clear().type('3,99')
+      cy.get(fuelSavings.tradePpg).clear().type('4,99')
+      cy.get(fuelSavings.milesDriven).clear().type('1000,0')
+      cy.get(fuelSavings.save).click
+      cy.get(fuelSavings.results).should('not.be.visible')
       }) 
 
 
-
-
       it('Teste de loss ou savings', () => { 
-        cy.get(fuelSavings.newMpg).clear().type(mpg1)
-        cy.get(fuelSavings.tradeMpg).clear().type(mpg2)
-        cy.get(fuelSavings.newPpg).clear().type(price1)
-        cy.get(fuelSavings.tradePpg).clear().type(price2)
-        cy.get(fuelSavings.milesDriven).clear().type(milhas)
-        cy.get(fuelSavings.dropMilesDriven).select('Year').should("have.value", "year")
-        cy.get(fuelSavings.save).click
+      cy.get(fuelSavings.newMpg).clear().type(mpg1)
+      cy.get(fuelSavings.tradeMpg).clear().type(mpg2)
+      cy.get(fuelSavings.newPpg).clear().type(price1)
+      cy.get(fuelSavings.tradePpg).clear().type(price2)
+      cy.get(fuelSavings.milesDriven).clear().type(milhas)
+      cy.get(fuelSavings.dropMilesDriven).select('Year').should("have.value", "year")
+      cy.get(fuelSavings.save).click
     
-          var x1 = (milhas / mpg1) * price1; 
-          var x2 = (milhas / mpg2) * price2;
-            if (x1 > x2){
-              cy.get(fuelSavings.results).should('contain', 'Loss')
-            }
-            else if (x1 <= x2)
-              cy.get(fuelSavings.results).should('contain', 'Savings')
+        var x1 = (milhas / mpg1) * price1; 
+        var x2 = (milhas / mpg2) * price2;
+          if (x1 > x2){
+            cy.get(fuelSavings.results).should('contain', 'Loss')
+          }
+          else if (x1 <= x2)
+            cy.get(fuelSavings.results).should('contain', 'Savings')
       })
-      
-    
-
-
-
-
-
 })
--- cypress/pages/fuelSavings.js
+++ cypress/pages/fuelSavings.js
@@ -1,5 +1,5 @@
 const fuelSavings = {
-
+    base_url: 'http://localhost:3000/fuel-savings',
     newMpg: 'input[name=newMpg',
     tradeMpg: 'input[name=tradeMpg]',
     newPpg: 'input[name=newPpg]',
